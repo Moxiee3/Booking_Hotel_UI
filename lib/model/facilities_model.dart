4c8c21fa327193a52ac3855834c70cc1963dfe85
unknown <ilhammaull>
2024-11-13 19:14:36 +0700

    first commit

---
+++ lib/model/facilities_model.dart
@@ -0,0 +1,20 @@
+class Facilities {
+  final String image, text;
+
+  Facilities({
+    required this.image,
+    required this.text,
+  });
+}
+
+List<Facilities> facilities = [
+  Facilities(image: 'image/bed.png', text: 'Bed'),
+  Facilities(image: 'image/food.png', text: 'Food'),
+  Facilities(image: 'image/hammock.png', text: 'Hammock'),
+  Facilities(image: 'image/music.png', text: 'Music'),
+  Facilities(image: 'image/sleeping-bag.png', text: 'Sleeping Bag'),
+  Facilities(image: 'image/t-shirt.png', text: 'T-Shirt'),
+  Facilities(image: 'image/tent.png', text: 'Tent'),
+  Facilities(image: 'image/tub.png', text: 'Tub'),
+  Facilities(image: 'image/wifi.png', text: 'Wifi'),
+];
---
+++ lib/screen/onboard_screen.dart
@@ -0,0 +1,105 @@
+import 'package:booking_hotel_app/color/colors.dart';
+import 'package:booking_hotel_app/model/onboard_model.dart';
+import 'package:booking_hotel_app/screen/hotel_home_screen.dart';
+import 'package:flutter/material.dart';
+
+class OnBoardPage extends StatefulWidget {
+  const OnBoardPage({super.key});
+
+  @override
+  State<OnBoardPage> createState() => _OnBoardPageState();
+}
+
+class _OnBoardPageState extends State<OnBoardPage> {
+  int currentIndex = 0;
+  Widget dotIndicator(int index) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 400),
+      margin: const EdgeInsets.only(right: 10),
+      width: index == currentIndex ? 40 : 10,
+      height: 10,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          color: index == currentIndex ? Colors.white : Colors.white54),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          PageView.builder(
+            onPageChanged: (value) => setState(() => currentIndex = value),
+            itemCount: onboards.length,
+            itemBuilder: (context, index) {
+              return Image.network(
+                onboards[index].image,
+                fit: BoxFit.cover,
+              );
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 60),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: List.generate(onboards.length, dotIndicator),
+                ),
+                const SizedBox(height: 20),
+                Text.rich(
+                  TextSpan(
+                    style: const TextStyle(
+                      fontSize: 45,
+                      height: 1.2,
+                      color: Colors.white,
+                    ),
+                    children: [
+                      TextSpan(
+                        text: "${onboards[currentIndex].text1}\n",
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w300,
+                        ),
+                      ),
+                      TextSpan(
+                        text: "${onboards[currentIndex].text2}\n",
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w900,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                if (currentIndex == onboards.length - 1)
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pushAndRemoveUntil(
+                          context,
+                          MaterialPageRoute(
+                            builder: (_) => const HotelHomeScreen(),
+                          ),
+                          (route) => false);
+                    },
+                    child: AnimatedContainer(
+                      padding: const EdgeInsets.all(15),
+                      duration: const Duration(milliseconds: 400),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: lightblue,
+                      ),
+                      child: const Icon(
+                        Icons.arrow_forward_ios,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
